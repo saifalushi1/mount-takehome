8493b471041b2ce43e628776be6865a17d602551
SaifSushi <alushisaif@gmail.com>
2022-06-21 00:48:43 -0500

    first commit

---
+++ src/main/java/com/mount/takehomechallenge/TakeHomeChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.mount.takehomechallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TakeHomeChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TakeHomeChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mount/takehomechallenge/TakeHomeChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mount.takehomechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakeHomeChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
